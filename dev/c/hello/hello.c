f742df93ab8acb21f75fdf34f23450dd36b0740f
jay trickett <jay@trickett.us>
2017-09-22 16:34:26 -0400

    rearrangin

---
+++ dev/c/hello/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main()
+{
+	printf("helllo, world\n");
+}
--- main_repo/static/js/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-http.createServer(function  (req, res) {
-    debugger;
-    var foo = 5 > 7 ? 'bar' : 'foo';
-    res.write(foo);
-    res.end();
-}).listen(3000);
