3f2d12de3cc88f8fb50ec98a41c675665723cb4d
Jason Trickett <jay@trickett.us>
2018-04-30 18:54:36 -0400

    removed frvr

--- devtools/frvr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /home/pool/poolio &&
-export NVM_DIR="/home/pool/.nvm" &&
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" &&
-nvm use 8 &&
-forever stop init.js &&
-forever start init.js
