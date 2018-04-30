6ce16260e81946f3b43386cf1e46a7fa5413f68a
Jason Trickett <jay@trickett.us>
2018-04-30 18:51:45 -0400

    slight mod to deploy

--- devtools/deploy.sh
+++ devtools/deploy.sh
@@ -4,7 +4,7 @@ scriptname=$0
 
 usage () {
 	cat <<EOF
-Useage: $scriptname [-s] [-r] appDirectory userName remoteHost
+Useage: $scriptname [-s] [-r] appDirectory userName remoteHost appUser
 	-s  starts the app after sync
 	-r  restarts currently running app after sync
 EOF
@@ -14,6 +14,7 @@ EOF
 appdir=$2
 user=$3
 remote=$4
+ruser=$5
 
 if [ "$1" = "-r" ]
 then
@@ -31,15 +32,15 @@ rsync -av ${appdir} ${remote}:/home/${user} &&
 echo \
 "
 #!/bin/sh\n
-rsync -av /home/${user}/${appdir} /home/pool && \n
-chown -R pool:pool /home/pool/${appdir} && \n
+rsync -av /home/${user}/${appdir} /home/${ruser} && \n
+chown -R ${ruser}:${ruser} /home/${ruser}/${appdir} && \n
 rm -rf /home/${user}/${appdir}"\
 > ./dep.sh &&
 
 echo \
 "
 #!/bin/sh\n
-export NVM_DIR="/home/pool/.nvm" && \n
+export NVM_DIR="/home/${ruser}/.nvm" && \n
 [ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\" && \n
 nvm use 8 && \n
 forever ${sr} init.js"\
@@ -51,11 +52,11 @@ scp ./frvr.sh ${remote}:/home/${user}/ &&
 sudoDep='
 sudo chmod +x /home/'"'${user}'"'/dep.sh && \ 
 sudo chmod +x /home/'"'${user}'"'/frvr.sh && \ 
-sudo mv /home/'"'${user}'"'/frvr.sh /home/pool/'"'${appdir}'"'/frvr.sh && \ 
+sudo mv /home/'"'${user}'"'/frvr.sh /home/'"'${ruser}'"'/'"'${appdir}'"'/frvr.sh && \ 
 sudo /home/'"'${user}'"'/dep.sh && \
 sudo rm /home/'"'${user}'"'/dep.sh
-cd /home/pool/'"'${appdir}'"' &&
-sudo -H -u pool "./frvr.sh"
+cd /home/'"'${ruser}'"'/'"'${appdir}'"' &&
+sudo -H -u '"'${ruser}'"' "./frvr.sh"
 '
 
 
